a2123a2dccd87a63e4aae5702be0fb4b4f09c3b9
Aziz <mu.aziz97@yahoo.com>
2022-03-16 09:58:16 +0700

    init

---
+++ twoSum.js
@@ -0,0 +1,26 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+
+    console.log(map);
+  }
+
+  return [];
+};
+
+console.log(twoSum([11, 4, 7, 14, 2, 5], 9));
