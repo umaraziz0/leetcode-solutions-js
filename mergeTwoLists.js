f37638d587f43e8ea2a1b2fa5e31195e762bbe0b
Aziz <mu.aziz97@yahoo.com>
2022-03-23 23:15:18 +0700

    add merge two lists

---
+++ mergeTwoLists.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  console.log(list1.val);
+  console.log(list2.val);
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
+
+// console.log(mergeTwoLists(null, null));
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+// console.log(mergeTwoLists([1, 2, 4], [1, 3, 4, 5]));
