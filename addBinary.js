11b942d40c9e6f811ed6ec365635e0e077867e18
Aziz <mu.aziz97@yahoo.com>
2022-04-06 20:51:06 +0700

    add addBinary

---
+++ addBinary.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/add-binary/
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  const sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
+
+  return sum.toString(2);
+};
+
+console.log(addBinary("11", "1"));
+console.log(addBinary("1010", "1011"));
